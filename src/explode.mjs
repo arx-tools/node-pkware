b1260a9b45bf9b15b864ea121776c855408704dc
Lajos.Meszaros <m_lajos@hotmail.com>
2020-07-04 11:51:55 +0200

    Moved first chunk parsing into it's own function

--- src/explode.mjs
+++ src/explode.mjs
@@ -1,4 +1,4 @@
-import { repeat, clone } from '../node_modules/ramda/src/index.mjs'
+import { repeat, mergeRight, clone } from '../node_modules/ramda/src/index.mjs'
 import {
   CMP_BAD_DATA,
   CMP_INVALID_DICTSIZE,
@@ -11,8 +11,41 @@ import { isBetween } from './helpers.mjs'
 
 const genAscTabs = state => {}
 
+const parseFirstChunk = chunk => {
+  return new Promise((resolve, reject) => {
+    const state = {}
+
+    if (chunk.length <= 4) {
+      reject(new Error(CMP_BAD_DATA))
+      return
+    }
+
+    state.compressionType = chunk.readUInt8(0)
+    state.dictionarySizeBits = chunk.readUInt8(1)
+    state.bitBuffer = chunk.readUIntBE(2, 2)
+
+    if (!isBetween(4, 6, state.dictionarySizeBits)) {
+      reject(new Error(CMP_INVALID_DICTSIZE))
+      return
+    }
+
+    state.dictionarySizeMask = 0xffff >> (0x10 - state.dictionarySizeBits)
+
+    if (state.compressionType !== BINARY_COMPRESSION) {
+      if (state.compressionType !== ASCII_COMPRESSION) {
+        reject(new Error(CMP_INVALID_MODE))
+        return
+      }
+      state.chBitsAsc = clone(ChBitsAsc)
+      genAscTabs(state)
+    }
+
+    resolve(state)
+  })
+}
+
 const explode = () => {
-  const state = {
+  let state = {
     isFirstChunk: true,
     compressionType: null,
     dictionarySizeBits: null,
@@ -24,39 +57,21 @@ const explode = () => {
   return (chunk, encoding, callback) => {
     if (state.isFirstChunk) {
       state.isFirstChunk = false
-      if (chunk.length <= 4) {
-        callback(new Error(CMP_BAD_DATA))
-        return
-      }
-
-      state.compressionType = chunk[0]
-      state.dictionarySizeBits = chunk[1]
-      state.bitBuffer = chunk.readUIntBE(2, 2)
-
-      if (!isBetween(4, 6, state.dictionarySizeBits)) {
-        callback(new Error(CMP_INVALID_DICTSIZE))
-        return
-      }
-
-      state.dictionarySizeMask = 0xffff >> (0x10 - state.dictionarySizeBits)
-
-      if (state.compressionType !== BINARY_COMPRESSION) {
-        if (state.compressionType !== ASCII_COMPRESSION) {
-          callback(new Error(CMP_INVALID_MODE))
-          return
-        }
-        state.chBitsAsc = clone(ChBitsAsc)
-        genAscTabs(state)
-      }
-
-      console.log(state)
-
+      parseFirstChunk(chunk)
+        .then(newState => {
+          state = mergeRight(state, newState)
+          console.log(state)
+          callback(null, chunk)
+        })
+        .catch(e => {
+          callback(e)
+        })
+    } else {
       callback(null, chunk)
-      return
     }
-
-    callback(null, chunk)
   }
 }
 
 export default explode
+
+export { genAscTabs, parseFirstChunk }
