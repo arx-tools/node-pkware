cc24268bc049082ce5f3d8cf181c0a8c14c08d3b
Mészáros Lajos <m_lajos@hotmail.com>
2021-01-02 04:25:10 +0100

    Added some TODOs

--- src/helpers/stream.js
+++ src/helpers/stream.js
@@ -1,1 +1,11 @@
+// TODO: create some higher order functions for streams
+//
+// 1) as a starter, we need an identity function: fn(streamHandler) == streamHandler
+//
+// 2) then we need an addIndex function, which adds +1 parameter to the handler: chunk index,
+// which is 0 for the first chunk, 1 for the second, etc
+// all of these have to preserve reference for "this"
+//
+// 3) then we need a universal function, like reduce
+
 module.exports = {}
