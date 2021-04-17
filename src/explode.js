de932749db9fdc3885a54988f3393a22a4c131e3
Mészáros Lajos <m_lajos@hotmail.com>
2021-04-17 03:00:33 +0200

    Added implementation for readHeader()

--- src/explode.js
+++ src/explode.js
@@ -1,7 +1,21 @@
-const { InvalidDataError } = require('./errors.js')
+const { InvalidDataError, InvalidCompressionTypeError, InvalidDictionarySizeError } = require('./errors.js')
+const { isBetween } = require('./helpers/functions.js')
 
-const readHeader = () => {
-  throw new InvalidDataError()
+const readHeader = buffer => {
+  if (!Buffer.isBuffer(buffer) || buffer.length < 4) {
+    throw new InvalidDataError()
+  }
+  if (buffer.readUInt8(0) !== 0 && buffer.readUInt8(0) !== 1) {
+    throw new InvalidCompressionTypeError()
+  }
+  if (!isBetween(4, 6, buffer.readUInt8(1))) {
+    throw new InvalidDictionarySizeError()
+  }
+
+  return {
+    compressionType: buffer.readUInt8(0),
+    dictionarySizeBits: buffer.readUInt8(1)
+  }
 }
 
 const explode = () => {
--- test/explode.spec.js
+++ test/explode.spec.js
@@ -2,25 +2,47 @@
 
 const assert = require('assert')
 const { isFunction, isPlainObject } = require('ramda-adjunct')
-const { InvalidDataError } = require('../src/errors.js')
+const { InvalidDataError, InvalidCompressionTypeError, InvalidDictionarySizeError } = require('../src/errors.js')
 const { explode, readHeader } = require('../src/explode.js')
 
 describe('readHeader', () => {
   it('is a function', () => {
     assert.ok(isFunction(readHeader), `${readHeader} is not a function`)
   })
-  it('throws an error if given parameter is not a Buffer or no parameter is given at all', () => {
+  it('throws an InvalidDataError if given parameter is not a Buffer or no parameter is given at all', () => {
     assert.throws(() => {
       readHeader()
-    })
+    }, InvalidDataError)
     assert.throws(() => {
       readHeader(100.9)
-    })
+    }, InvalidDataError)
+    assert.throws(() => {
+      readHeader([1, 2, 3, 4, 5])
+    }, InvalidDataError)
   })
   it('throws an InvalidDataError, when given Buffer is less, than 4 bytes', () => {
     assert.throws(() => {
       readHeader(Buffer.from([1, 2, 3]))
     }, InvalidDataError)
+  })
+  it('throws an InvalidCompressionTypeError, when first byte of given Buffer is neither 0, nor 1', () => {
+    assert.throws(() => {
+      readHeader(Buffer.from([7, 1, 2, 3]))
+    }, InvalidCompressionTypeError)
+  })
+  it('throws an InvalidDictionarySizeError, when second byte of given Buffer is not between 4 and 6', () => {
+    assert.throws(() => {
+      readHeader(Buffer.from([1, 9, 2, 3]))
+    }, InvalidDictionarySizeError)
+  })
+  it('returns an object', () => {
+    const state = readHeader(Buffer.from([0x00, 0x04, 0x86, 0xbc]))
+    assert.ok(isPlainObject(state), `${state} is not an object`)
+  })
+  it('loads the bytes of the Buffer to the state ', () => {
+    const state = readHeader(Buffer.from([0x00, 0x04, 0x86, 0xbc]))
+    assert.strictEqual(state.compressionType, 0x00)
+    assert.strictEqual(state.dictionarySizeBits, 0x04)
   })
 })
 
