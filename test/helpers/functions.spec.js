21d8e3a414ceeea9775f51214ce95789e0e9b125
Lajos Meszaros <m_lajos@hotmail.com>
2022-10-14 10:45:44 +0200

    fix test message for negative case

--- test/helpers/functions.spec.js
+++ test/helpers/functions.spec.js
@@ -21,7 +21,7 @@ describe('helpers/functions', () => {
       assert.strictEqual(isBetween(3, 7, 4), true)
       assert.strictEqual(isBetween(-5, 0, -1), true)
     })
-    it('takes 3 numbers and returns false when the 3rd number is between the first 2', () => {
+    it('takes 3 numbers and returns false when the 3rd number is not between the first 2', () => {
       assert.strictEqual(isBetween(3, 7, 94), false)
     })
     it('returns null, when any of the parameters are not numbers', () => {
