665c2f1481035397378965e3b1326ad1ae835fb3
Lajos Meszaros <m_lajos@hotmail.com>
2022-12-22 00:09:02 +0100

    replace private with # prefix

--- src/ExpandingBuffer.ts
+++ src/ExpandingBuffer.ts
@@ -2,31 +2,24 @@ import { Buffer } from 'node:buffer'
 import { clamp } from './functions'
 
 export class ExpandingBuffer {
-  private heap: Buffer
-  private startIndex: number
-  private endIndex: number
-  private backup: {
-    startIndex: number
-    endIndex: number
+  #heap: Buffer
+  #startIndex: number = 0
+  #endIndex: number = 0
+  #backup: { startIndex: number; endIndex: number } = {
+    startIndex: 0,
+    endIndex: 0,
   }
 
   constructor(numberOfBytes: number = 0) {
-    this.heap = Buffer.allocUnsafe(numberOfBytes)
-    this.startIndex = 0
-    this.endIndex = 0
-
-    this.backup = {
-      startIndex: 0,
-      endIndex: 0,
-    }
+    this.#heap = Buffer.allocUnsafe(numberOfBytes)
   }
 
-  private getActualData(offset: number = 0) {
-    return this.heap.subarray(this.startIndex + offset, this.endIndex)
+  #getActualData(offset: number = 0) {
+    return this.#heap.subarray(this.#startIndex + offset, this.#endIndex)
   }
 
   size() {
-    return this.endIndex - this.startIndex
+    return this.#endIndex - this.#startIndex
   }
 
   isEmpty() {
@@ -34,37 +27,37 @@ export class ExpandingBuffer {
   }
 
   heapSize() {
-    return this.heap.length
+    return this.#heap.length
   }
 
   append(buffer: Buffer) {
-    if (this.endIndex + buffer.length < this.heapSize()) {
-      buffer.copy(this.heap, this.endIndex)
-      this.endIndex += buffer.length
+    if (this.#endIndex + buffer.length < this.heapSize()) {
+      buffer.copy(this.#heap, this.#endIndex)
+      this.#endIndex += buffer.length
     } else {
-      this.heap = Buffer.concat([this.getActualData(), buffer])
-      this.startIndex = 0
-      this.endIndex = this.heapSize()
+      this.#heap = Buffer.concat([this.#getActualData(), buffer])
+      this.#startIndex = 0
+      this.#endIndex = this.heapSize()
     }
   }
 
   /**
-   * Watch out! The returned Buffer points to the same memory!
+   * Watch out! The returned slice of Buffer points to the same Buffer in memory!
    */
-  read(offset: number, limit: number) {
+  read(offset: number = 0, limit: number = this.size()) {
     if (offset < 0 || limit < 1) {
       return Buffer.from([])
     }
 
     if (limit === 1) {
-      return this.heap[this.startIndex + offset]
+      return this.#heap[this.#startIndex + offset]
     }
 
     if (offset + limit < this.size()) {
-      return this.heap.subarray(this.startIndex + offset, this.startIndex + limit + offset)
+      return this.#heap.subarray(this.#startIndex + offset, this.#startIndex + limit + offset)
     }
 
-    return this.getActualData(offset)
+    return this.#getActualData(offset)
   }
 
   /**
@@ -78,11 +71,11 @@ export class ExpandingBuffer {
     numberOfBytes = clamp(0, this.heapSize(), numberOfBytes)
     if (numberOfBytes > 0) {
       if (numberOfBytes < this.heapSize()) {
-        this.heap.copy(this.heap, 0, this.startIndex + numberOfBytes)
+        this.#heap.copy(this.#heap, 0, this.#startIndex + numberOfBytes)
       }
 
-      this.endIndex -= this.startIndex + numberOfBytes
-      this.startIndex = 0
+      this.#endIndex -= this.#startIndex + numberOfBytes
+      this.#startIndex = 0
     }
   }
 
@@ -95,7 +88,7 @@ export class ExpandingBuffer {
   flushEnd(numberOfBytes: number) {
     const clampedNumberOfBytes = clamp(0, this.heapSize(), numberOfBytes)
     if (clampedNumberOfBytes > 0) {
-      this.endIndex -= clampedNumberOfBytes
+      this.#endIndex -= clampedNumberOfBytes
     }
   }
 
@@ -108,8 +101,8 @@ export class ExpandingBuffer {
    */
   dropStart(numberOfBytes: number) {
     if (numberOfBytes > 0) {
-      this.startIndex += numberOfBytes
-      if (this.startIndex >= this.endIndex) {
+      this.#startIndex += numberOfBytes
+      if (this.#startIndex >= this.#endIndex) {
         this.clear()
       }
     }
@@ -124,8 +117,8 @@ export class ExpandingBuffer {
    */
   dropEnd(numberOfBytes: number) {
     if (numberOfBytes > 0) {
-      this.endIndex -= numberOfBytes
-      if (this.startIndex >= this.endIndex) {
+      this.#endIndex -= numberOfBytes
+      if (this.#startIndex >= this.#endIndex) {
         this.clear()
       }
     }
@@ -135,21 +128,21 @@ export class ExpandingBuffer {
    * returns the internal buffer
    */
   getHeap() {
-    return this.heap
+    return this.#heap
   }
 
   clear() {
-    this.startIndex = 0
-    this.endIndex = 0
+    this.#startIndex = 0
+    this.#endIndex = 0
   }
 
   saveIndices() {
-    this.backup.startIndex = this.startIndex
-    this.backup.endIndex = this.endIndex
+    this.#backup.startIndex = this.#startIndex
+    this.#backup.endIndex = this.#endIndex
   }
 
   restoreIndices() {
-    this.startIndex = this.backup.startIndex
-    this.endIndex = this.backup.endIndex
+    this.#startIndex = this.#backup.startIndex
+    this.#endIndex = this.#backup.endIndex
   }
 }
