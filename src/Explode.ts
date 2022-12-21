436461aaeb504a0640fde28bcf76f510937df62f
Lajos Meszaros <m_lajos@hotmail.com>
2022-12-21 23:48:28 +0100

    use the new # syntax from JS for proper privates

--- src/Explode.ts
+++ src/Explode.ts
@@ -6,74 +6,74 @@ import { repeat, toHex } from './functions'
 import { Config, Stats } from './types'
 
 export class Explode {
-  verbose: boolean
-  needMoreInput: boolean = true
-  isFirstChunk: boolean = true
-  extraBits: number = 0
-  bitBuffer: number = 0
-  backupExtraBits: number = -1
-  backupBitBuffer: number = -1
-  chBitsAsc: number[] = repeat(0, 0x100)
-  // lengthCodes
-  // distPosCodes
-  inputBuffer: ExpandingBuffer
-  outputBuffer: ExpandingBuffer
-  stats: Stats = { chunkCounter: 0 }
+  #verbose: boolean
+  #needMoreInput: boolean = true
+  #isFirstChunk: boolean = true
+  #extraBits: number = 0
+  #bitBuffer: number = 0
+  #backupExtraBits: number = -1
+  #backupBitBuffer: number = -1
+  #chBitsAsc: number[] = repeat(0, 0x100)
+  // #lengthCodes
+  // #distPosCodes
+  #inputBuffer: ExpandingBuffer
+  #outputBuffer: ExpandingBuffer
+  #stats: Stats = { chunkCounter: 0 }
 
   constructor(config: Config = {}) {
-    this.verbose = config?.verbose ?? false
-    this.inputBuffer = new ExpandingBuffer(config?.inputBufferSize ?? 0)
-    this.outputBuffer = new ExpandingBuffer(config?.outputBufferSize ?? 0)
+    this.#verbose = config?.verbose ?? false
+    this.#inputBuffer = new ExpandingBuffer(config?.inputBufferSize ?? 0)
+    this.#outputBuffer = new ExpandingBuffer(config?.outputBufferSize ?? 0)
   }
 
-  onInputFinished(callback: TransformCallback) {
-    if (this.verbose) {
+  getHandler() {
+    const instance = this
+
+    return function (this: Transform, chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback) {
+      instance.#needMoreInput = true
+
+      try {
+        instance.#inputBuffer.append(chunk)
+        if (instance.#isFirstChunk) {
+          instance.#isFirstChunk = false
+          this._flush = instance.#onInputFinished.bind(instance)
+        }
+
+        if (instance.#verbose) {
+          instance.#stats.chunkCounter++
+          console.log(`explode: reading ${toHex(chunk.length)} bytes from chunk #${instance.#stats.chunkCounter}`)
+        }
+
+        // TODO: migrate the rest of the function
+      } catch (e: unknown) {
+        callback(e as Error)
+      }
+    }
+  }
+
+  #onInputFinished(callback: TransformCallback) {
+    if (this.#verbose) {
       console.log('---------------')
-      console.log('explode: total number of chunks read:', this.stats.chunkCounter)
-      console.log('explode: inputBuffer heap size', toHex(this.inputBuffer.heapSize()))
-      console.log('explode: outputBuffer heap size', toHex(this.outputBuffer.heapSize()))
+      console.log('explode: total number of chunks read:', this.#stats.chunkCounter)
+      console.log('explode: inputBuffer heap size', toHex(this.#inputBuffer.heapSize()))
+      console.log('explode: outputBuffer heap size', toHex(this.#outputBuffer.heapSize()))
     }
 
-    if (this.needMoreInput) {
+    if (this.#needMoreInput) {
       callback(new AbortedError())
       return
     }
   }
 
-  backup() {
-    this.backupExtraBits = this.extraBits
-    this.backupBitBuffer = this.bitBuffer
-    this.inputBuffer.saveIndices()
+  #backup() {
+    this.#backupExtraBits = this.#extraBits
+    this.#backupBitBuffer = this.#bitBuffer
+    this.#inputBuffer.saveIndices()
   }
 
-  restore() {
-    this.extraBits = this.backupExtraBits
-    this.bitBuffer = this.backupBitBuffer
-    this.inputBuffer.restoreIndices()
-  }
-}
-
-const explode = (config: Config = {}) => {
-  const self = new Explode(config)
-
-  return function (this: Transform, chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback) {
-    self.needMoreInput = true
-
-    try {
-      self.inputBuffer.append(chunk)
-      if (self.isFirstChunk) {
-        self.isFirstChunk = false
-        this._flush = self.onInputFinished.bind(self)
-      }
-
-      if (self.verbose) {
-        self.stats.chunkCounter++
-        console.log(`explode: reading ${toHex(chunk.length)} bytes from chunk #${self.stats.chunkCounter}`)
-      }
-
-      // TODO: migrate the rest of the function
-    } catch (e: unknown) {
-      callback(e as Error)
-    }
+  #restore() {
+    this.#extraBits = this.#backupExtraBits
+    this.#bitBuffer = this.#backupBitBuffer
+    this.#inputBuffer.restoreIndices()
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-export { implode } from './_implode'
-export { implode as compress } from './_implode'
-export { explode } from './_explode'
-export { explode as decompress } from './_explode'
+import { Explode } from './Explode'
+import { Config } from './types'
 
-export * as constants from './constants'
-export * as errors from './errors'
-export * as stream from './helpers/stream'
+export const explode = (config: Config = {}) => {
+  const instance = new Explode(config)
+  return instance.getHandler()
+}
+export { explode as decompress }
+
+// export { implode } from './_implode'
+// export { implode as compress } from './_implode'
+
+// export * as constants from './constants'
+// export * as errors from './errors'
+// export * as stream from './helpers/stream'
