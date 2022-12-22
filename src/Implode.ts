69d57e75e95d47de339b644bd3d159226cde5059
Lajos Meszaros <m_lajos@hotmail.com>
2022-12-22 10:33:22 +0100

    add implode function and improve the Implode class a bit

--- src/Implode.ts
+++ src/Implode.ts
@@ -1,12 +1,25 @@
 import { Buffer } from 'node:buffer'
-import { Callback, Config } from './types'
+import { Transform, TransformCallback } from 'node:stream'
+import { ExpandingBuffer } from './ExpandingBuffer'
+import { Config, Stats } from './types'
 
 export class Implode {
-  private verbose: boolean
+  #verbose: boolean
+  #inputBuffer: ExpandingBuffer
+  #outputBuffer: ExpandingBuffer
+  #stats: Stats = { chunkCounter: 0 }
 
   constructor(config: Config) {
-    this.verbose = config?.verbose ?? false
+    this.#verbose = config?.verbose ?? false
+    this.#inputBuffer = new ExpandingBuffer(config?.inputBufferSize ?? 0)
+    this.#outputBuffer = new ExpandingBuffer(config?.outputBufferSize ?? 0)
   }
 
-  handler(chunk: Buffer, encoding: string, callback: Callback) {}
+  getHandler() {
+    const instance = this
+
+    return function (this: Transform, chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback) {
+      // TODO: implement the rest of the handler
+    }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { Explode } from './Explode'
+import { Implode } from './Implode'
 import { Config } from './types'
 
 export const explode = (config: Config = {}) => {
   const instance = new Explode(config)
   return instance.getHandler()
 }
-export { explode as decompress }
 
-// export { implode } from './_implode'
-// export { implode as compress } from './_implode'
+export const implode = (config: Config = {}) => {
+  const instance = new Implode(config)
+  return instance.getHandler()
+}
+
+export { explode as decompress }
+export { implode as compress }
 
 // export * as constants from './constants'
 // export * as errors from './errors'
