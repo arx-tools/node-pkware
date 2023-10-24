5e75119e5571972c5524231bf10c91a78a09ea44
Lajos Meszaros <m_lajos@hotmail.com>
2023-10-24 17:38:47 +0200

    docs(constants): compress jsdoc block

--- src/constants.ts
+++ src/constants.ts
@@ -10,8 +10,10 @@ export enum Compression {
 /**
  * Dictionary sizes for implode, determines how well the file get compressed.
  *
- * Small dictionary size means less memory to lookback in data for repetitions, meaning it will be less effective, the file stays larger, less compressed.
- * On the other hand, large compression allows more lookback allowing more effective compression, thus generating smaller, more compressed files.
+ * Small dictionary size means less memory to lookback in data for repetitions,
+ * meaning it will be less effective, the file stays larger, less compressed.
+ * On the other hand, large compression allows more lookback allowing more effective
+ * compression, thus generating smaller, more compressed files.
  */
 export enum DictionarySize {
   Unknown = -1,
