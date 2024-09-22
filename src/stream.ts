61594a14e7732bfff37fd9fd2271e7cab2d36c4e
Lajos Meszaros <m_lajos@hotmail.com>
2024-09-22 22:13:14 +0200

    refactor(stream): create TransformPredicate type to describe what functions like splitAt should return

--- src/stream.ts
+++ src/stream.ts
@@ -11,6 +11,8 @@ export type StreamHandler = (
   callback: TransformCallback,
 ) => void | Promise<void>
 
+type TransformPredicate = (chunk: Buffer) => [Buffer, Buffer, boolean]
+
 class QuasiTransform {
   _flush?: (callback: TransformCallback) => void
 
@@ -26,17 +28,22 @@ class QuasiTransform {
 }
 
 /**
- * Creates a "**predicate**" function, that awaits Buffers, keeps an internal counter of the bytes from them and splits the appropriate buffer at the given index.
+ * Creates a {@link TransformPredicate} function, that awaits Buffers,
+ * keeps an internal counter of the bytes from them and splits the appropriate buffer at the given index.
+ *
  * Splitting is done by returning an array with `[left: Buffer, right: Buffer, isLeftDone: bool]`.
- * If you want to split data at the 100th byte and you keep feeding 60 byte long buffers to the function returned by `splitAt(100)`, then it will return arrays in the following manner:
+ *
+ * If you want to split data at the 100th byte and you keep feeding 60 byte long buffers to the function returned by
+ * `splitAt(100)`, then it will return arrays in the following manner:
  * 1. `[inputBuffer, emptyBuffer, false]`
  * 2. `[inputBuffer.slice(0, 40), inputBuffer.slice(40, 60), true]`
  * 3. `[emptyBuffer, inputBuffer, true]`
  * 4. `[emptyBuffer, inputBuffer, true]`
  * 5. ... and so on
+ *
  * @param index - a positive integer at which to split the buffer
  */
-export function splitAt(index: number) {
+export function splitAt(index: number): TransformPredicate {
   let cntr = 0
 
   return function (chunk: Buffer): [Buffer, Buffer, boolean] {
@@ -102,7 +109,7 @@ export function through(handler: StreamHandler): Transform {
  * @returns a `transform._transform` type function
  */
 export function transformSplitBy(
-  predicate: (chunk: Buffer) => [Buffer, Buffer, boolean],
+  predicate: TransformPredicate,
   leftHandler: StreamHandler,
   rightHandler: StreamHandler,
 ): StreamHandler {
