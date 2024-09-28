3644da3015badec17c1934545f943e7dc572df41
Lajos Meszaros <m_lajos@hotmail.com>
2024-09-28 23:55:07 +0200

    refactor(bin): fix linter errors

--- src/bin/implode.ts
+++ src/bin/implode.ts
@@ -4,7 +4,14 @@ import process from 'node:process'
 import minimist from 'minimist-lite'
 import { Compression, DictionarySize } from '@src/constants.js'
 import { getPackageVersion, parseNumberString, getInputStream, getOutputStream } from '@bin/helpers.js'
-import { transformEmpty, transformIdentity, transformSplitBy, splitAt, through } from '@src/stream.js'
+import {
+  transformEmpty,
+  transformIdentity,
+  transformSplitBy,
+  splitAt,
+  through,
+  type StreamHandler,
+} from '@src/stream.js'
 import { type Config } from '@src/types.js'
 import { implode } from '@src/index.js'
 
@@ -43,7 +50,7 @@ const args: AppArgs = minimist(process.argv.slice(2), {
   },
 })
 
-const compress = (
+async function compress(
   input: NodeJS.ReadableStream,
   output: NodeJS.WritableStream,
   offset: number,
@@ -51,67 +58,96 @@ const compress = (
   compressionType: Compression,
   dictionarySize: DictionarySize,
   config: Config,
-) => {
-  const leftHandler = keepHeader ? transformIdentity() : transformEmpty()
+): Promise<void> {
+  let leftHandler: StreamHandler
+  if (keepHeader) {
+    leftHandler = transformIdentity()
+  } else {
+    leftHandler = transformEmpty()
+  }
+
   const rightHandler = implode(compressionType, dictionarySize, config)
 
   const handler = transformSplitBy(splitAt(offset), leftHandler, rightHandler)
 
-  return new Promise((resolve, reject) => {
+  return new Promise<void>((resolve, reject) => {
     input.pipe(through(handler).on('error', reject)).pipe(output).on('finish', resolve).on('error', reject)
   })
 }
 
-;(async () => {
-  if (args.version) {
-    const version = await getPackageVersion()
-    console.log(`node-pkware - version ${version}`)
-    process.exit(0)
+if (args.version) {
+  const version = await getPackageVersion()
+  console.log(`node-pkware - version ${version}`)
+  process.exit(0)
+}
+
+let input: NodeJS.ReadableStream
+let output: NodeJS.WritableStream
+try {
+  if (!args.ascii && !args.binary) {
+    throw new Error('compression type missing, expected either --ascii or --binary')
   }
 
-  let input: NodeJS.ReadableStream
-  let output: NodeJS.WritableStream
-  try {
-    if (!args.ascii && !args.binary) {
-      throw new Error('compression type missing, expected either --ascii or --binary')
-    }
-
-    if (args.ascii && args.binary) {
-      throw new Error('multiple compression types specified, can only work with either --ascii or --binary')
-    }
-
-    if (!args.small && !args.medium && !args.large) {
-      throw new Error('dictionary size missing, expected either --small, --medium or --large')
-    }
-
-    if ((args.small ? 1 : 0) + (args.medium ? 1 : 0) + (args.large ? 1 : 0) > 1) {
-      throw new Error('multiple dictionary sizes specified, can only work with either --small, --medium or --large')
-    }
-
-    input = await getInputStream(args._[0])
-    output = await getOutputStream(args.output)
-  } catch (e: unknown) {
-    const error = e as Error
-    console.error('error:', error.message)
-    process.exit(1)
+  if (args.ascii && args.binary) {
+    throw new Error('multiple compression types specified, can only work with either --ascii or --binary')
   }
 
-  const compressionType = args.ascii ? Compression.Ascii : Compression.Binary
-  const dictionarySize = args.small ? DictionarySize.Small : args.medium ? DictionarySize.Medium : DictionarySize.Large
-  const offset = parseNumberString(args.offset, 0)
-  const keepHeader = !args['drop-before-offset']
-  const config: Config = {
-    verbose: args.verbose,
-    inputBufferSize: parseNumberString(args['input-buffer-size'], 0x10000),
-    outputBufferSize: parseNumberString(args['output-buffer-size'], 0x12000),
+  if (!args.small && !args.medium && !args.large) {
+    throw new Error('dictionary size missing, expected either --small, --medium or --large')
   }
 
-  compress(input, output, offset, keepHeader, compressionType, dictionarySize, config)
-    .then(() => {
-      process.exit(0)
-    })
-    .catch((e) => {
-      console.error(`error: ${e.message}`)
-      process.exit(1)
-    })
-})()
+  let numberOfDictionarySizeArguments = 0
+  if (args.small) {
+    numberOfDictionarySizeArguments = numberOfDictionarySizeArguments + 1
+  }
+
+  if (args.medium) {
+    numberOfDictionarySizeArguments = numberOfDictionarySizeArguments + 1
+  }
+
+  if (args.large) {
+    numberOfDictionarySizeArguments = numberOfDictionarySizeArguments + 1
+  }
+
+  if (numberOfDictionarySizeArguments > 1) {
+    throw new Error('multiple dictionary sizes specified, can only work with either --small, --medium or --large')
+  }
+
+  input = await getInputStream(args._[0])
+  output = await getOutputStream(args.output)
+} catch (error: unknown) {
+  console.error('error:', (error as Error).message)
+  process.exit(1)
+}
+
+let compressionType
+if (args.ascii) {
+  compressionType = Compression.Ascii
+} else {
+  compressionType = Compression.Binary
+}
+
+let dictionarySize
+if (args.small) {
+  dictionarySize = DictionarySize.Small
+} else if (args.medium) {
+  dictionarySize = DictionarySize.Medium
+} else {
+  dictionarySize = DictionarySize.Large
+}
+
+const offset = parseNumberString(args.offset, 0)
+const keepHeader = !args['drop-before-offset']
+const config: Config = {
+  verbose: args.verbose,
+  inputBufferSize: parseNumberString(args['input-buffer-size'], 0x1_00_00),
+  outputBufferSize: parseNumberString(args['output-buffer-size'], 0x1_20_00),
+}
+
+try {
+  await compress(input, output, offset, keepHeader, compressionType, dictionarySize, config)
+  process.exit(0)
+} catch (error: unknown) {
+  console.error('error:', (error as Error).message)
+  process.exit(1)
+}
