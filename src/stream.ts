e57699d4f580c4d480f5133f524944db9d0f8b77
Lajos Meszaros <m_lajos@hotmail.com>
2024-09-28 12:42:38 +0200

    refactor(stream): rename variable

--- src/stream.ts
+++ src/stream.ts
@@ -28,9 +28,13 @@ class QuasiTransform {
 }
 
 /**
- * Creates a {@link TransformPredicate} function, that awaits Buffers, keeps an internal counter of the bytes from them and splits the appropriate buffer at the given index.
+ * Creates a {@link TransformPredicate} function, that awaits Buffers, keeps an internal counter
+ * of the bytes from them and splits the appropriate buffer at the given index.
+ *
  * Splitting is done by returning an array with `[left: Buffer, right: Buffer, isLeftDone: bool]`.
- * If you want to split data at the 100th byte and you keep feeding 60 byte long buffers to the function returned by `splitAt(100)`, then it will return arrays in the following manner:
+ *
+ * If you want to split data at the 100th byte and you keep feeding 60 byte long buffers to the function
+ * returned by `splitAt(100)`, then it will return arrays in the following manner:
  * 1. `[inputBuffer, emptyBuffer, false]`
  * 2. `[inputBuffer.slice(0, 40), inputBuffer.slice(40, 60), true]`
  * 3. `[emptyBuffer, inputBuffer, true]`
@@ -135,9 +139,9 @@ export function transformSplitBy(
             return
           }
 
-          leftTransform._flush((err, data) => {
-            if (err) {
-              reject(err)
+          leftTransform._flush((error, data) => {
+            if (error) {
+              reject(error)
             } else {
               resolve(data)
             }
